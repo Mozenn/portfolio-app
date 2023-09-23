fcf1e6da064aa1a37749bcbc7bae5c108b0f675f
Mohzenn <gauthier.cassany@gmail.com>
2023-09-23 15:43:30 +0200

    refactor cypress tests

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,5 +3,7 @@ import { defineConfig } from 'cypress';
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
+    specPattern: 'cypress/integration/**/*.cy.js',
+    supportFile: 'cypress/support/e2e.js',
   },
 });
